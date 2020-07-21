39e353461c683ea6b94ae795a15d79cdf1b7a595
Laura <lauraqpalacios@gmail.com>
2020-07-21 17:53:50 -0500

    Calculadora corporal

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let peso = prompt("Ingresa tu peso (kg): ");
+let altura = prompt("Ingresa tu altura (m): ");
+
+function IMC (peso , altura){
+    let resultado = peso / (altura^2);
+
+    console.log("Tu índice de masa corporal es: " + resultado);
+    document.write("Tu índice de masa corporal es: " + resultado);
+    alert("Tu índice de masa corporal es: " + resultado);
+}
+
+IMC(peso , altura)
+
+
